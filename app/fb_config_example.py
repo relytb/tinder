88a2be7da1c83ea01d299350de0df355a8d89b94
relytb <ty.bondy@gmail.com>
2018-12-16 09:18:55 -0500

    Fix authorization bug

--- app/fb_config_example.py
+++ app/fb_config_example.py
@@ -8,5 +8,9 @@ fb_access_token = None
 fb_user_id = None
 
 def init():
-    fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
-    fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
+    global fb_access_token
+    if fb_access_token == None:
+        fb_access_token = fb_auth_token.get_fb_access_token(fb_username, fb_password)
+    global fb_user_id
+    if fb_user_id == None:
+        fb_user_id = fb_auth_token.get_fb_id(fb_access_token)
